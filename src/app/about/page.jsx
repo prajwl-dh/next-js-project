fd9a4ae37cbfe015757fac351d8993013089d958
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2024-01-07 11:48:35 -0600

    Created and styled contact page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -5,9 +5,9 @@ export default function About(){
     return(
         <div className={styles.container}>
             <div className={styles.textContainer}>
-                <h2>About Agency</h2>
-                <h1> We create digital ideas that are bigger, bolder, braver and better.</h1>
-                <p>We create digital ideas that are bigger, bolder, braver and better.
+                <h2 className={styles.subtitle}>About Agency</h2>
+                <h1 className={styles.title}> We create digital ideas that are bigger, bolder, braver and better.</h1>
+                <p className={styles.description}>We create digital ideas that are bigger, bolder, braver and better.
                 We create digital ideas that are bigger, bolder, braver and better.
                 We create digital ideas that are bigger, bolder, braver and better.
                 We create digital ideas that are bigger, bolder, braver and better.
@@ -18,10 +18,20 @@ export default function About(){
                         <h1>10 K+</h1>
                         <p>Years of Experience</p>
                     </div>
+                    <div className={styles.box}>
+                        <h1>10 K+</h1>
+                        <p>Years of Experience</p>
+                    </div>
+                    <div className={styles.box}>
+                        <h1>10 K+</h1>
+                        <p>Years of Experience</p>
+                    </div>
                 </div>
+                
             </div>
             <div className={styles.imgContainer}>
-                <Image 
+                <Image
+                className={styles.img} 
                     src='/about.png'
                     alt=''
                     fill
